b984f9dbd1ceaa7a36a8344f5c6d39b9f18c8326
Joel Davalos <jsdavalos@gmail.com>
2024-05-14 17:41:56 -0600

    Deck class created

---
+++ lib/deck.rb
@@ -0,0 +1,26 @@
+class Deck
+    attr_reader :cards
+    def initialize(cards)
+     @cards = cards
+    end
+  
+    def rank_of_card_at(index)
+      @cards[index].rank
+    end
+  
+    def high_ranking_cards
+      @cards.select { |card| card.rank >=11 }
+    end
+  
+    def perecnt_high_ranking
+      (high_ranking_cards.length.to.f / @cards.length * 100)
+    end 
+    
+    def remove_card
+      @cards.shift
+    end
+  
+    def add_card(card)
+      @cards << card
+    end
+  end
